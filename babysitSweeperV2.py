3785cab1c443a9db8581b2300001f82677067530
weirdge <peepodone+github@gmail.com>
2026-03-17 22:50:10 +0100

    + spawn delay (server not that laggy that way) + github link + less bots (2x less bots and better bot alignment)

--- babysitSweeperV2.py
+++ babysitSweeperV2.py
@@ -1,6 +1,7 @@
 import minescript
 import math
 import sys
+import time
 def exit():
     minescript.execute("/tellraw @s {\"text\":\"Please supply a name\",\"color\":\"green\"}")
     minescript.execute("/tellraw @a [\"Usage: \",{\"text\":\"babysitV2 \",\"color\":\"aqua\"},{\"text\":\"<name>\",\"color\":\"dark_green\"}]")
@@ -33,21 +34,25 @@ if relativeRotation < 45 and relativeRotation > -45: # looking south
     print(f"looking south")
     for i in range(abs(math.ceil((weStart-position[2])/(renderDistanceBlocks)/2))):
         minescript.execute(f"/player {botName}{i} spawn at {position[0]} {botHeight} {position[2]+i*renderDistanceBlocks*2}")
+        time.sleep(0.3)
 
 elif relativeRotation < -45 and relativeRotation > -135: # looking east
     print(f"looking east")
     for i in range(abs(math.ceil((weStart-position[0])/(renderDistanceBlocks)/2))):
         minescript.execute(f"/player {botName}{i} spawn at {position[0]+i*renderDistanceBlocks*2} {botHeight} {position[2]}")
+        time.sleep(0.3)
 
 elif relativeRotation > 45 and relativeRotation < 135: # looking west
     print(f"looking west")
     for i in range(abs(math.ceil((weStart-weLength-position[0])/(renderDistanceBlocks)/2))):
         minescript.execute(f"/player {botName}{i} spawn at {position[0]-i*renderDistanceBlocks*2} {botHeight} {position[2]}")
+        time.sleep(0.3)
 
 elif relativeRotation < -135 or relativeRotation > 135: # looking north
     print(f"looking north")
     for i in range(abs(math.ceil((weStart-weLength-position[2])/(renderDistanceBlocks)/2))):
         minescript.execute(f"/player {botName}{i} spawn at {position[0]} {botHeight} {position[2]-i*renderDistanceBlocks*2}")
+        time.sleep(0.3)
 
 else: # something else
-    print(f"looking at transition.\nPlease rotate a little.\n\nIf this is not the case, please contact me")
+    print(f"looking at transition.\nPlease rotate a little.\n\nIf there's anything else please open an issue.\nGithub: https://github.com/weirdge/minescript/issues")
