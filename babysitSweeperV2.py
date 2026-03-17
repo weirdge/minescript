05adc4115571118f19467bc6345e590fcd55a5b3
weirdge <peepodone+github@gmail.com>
2026-03-17 23:11:08 +0100

    + small bot alignment changes

--- babysitSweeperV2.py
+++ babysitSweeperV2.py
@@ -33,25 +33,25 @@ print(relativeRotation)
 if relativeRotation < 45 and relativeRotation > -45: # looking south
     print(f"looking south")
     for i in range(abs(math.ceil((weStart-position[2])/(renderDistanceBlocks)/2))):
-        minescript.execute(f"/player {botName}{i} spawn at {position[0]} {botHeight} {position[2]+i*renderDistanceBlocks*2}")
+        minescript.execute(f"/player {botName}{i} spawn at {position[0]} {botHeight} {position[2]+(i*renderDistanceBlocks*2)+renderDistanceBlocks}")
         time.sleep(0.3)
 
 elif relativeRotation < -45 and relativeRotation > -135: # looking east
     print(f"looking east")
     for i in range(abs(math.ceil((weStart-position[0])/(renderDistanceBlocks)/2))):
-        minescript.execute(f"/player {botName}{i} spawn at {position[0]+i*renderDistanceBlocks*2} {botHeight} {position[2]}")
+        minescript.execute(f"/player {botName}{i} spawn at {position[0]+(i*renderDistanceBlocks*2)+renderDistanceBlocks} {botHeight} {position[2]}")
         time.sleep(0.3)
 
 elif relativeRotation > 45 and relativeRotation < 135: # looking west
     print(f"looking west")
     for i in range(abs(math.ceil((weStart-weLength-position[0])/(renderDistanceBlocks)/2))):
-        minescript.execute(f"/player {botName}{i} spawn at {position[0]-i*renderDistanceBlocks*2} {botHeight} {position[2]}")
+        minescript.execute(f"/player {botName}{i} spawn at {position[0]-(i*renderDistanceBlocks*2)-renderDistanceBlocks} {botHeight} {position[2]}")
         time.sleep(0.3)
 
 elif relativeRotation < -135 or relativeRotation > 135: # looking north
     print(f"looking north")
     for i in range(abs(math.ceil((weStart-weLength-position[2])/(renderDistanceBlocks)/2))):
-        minescript.execute(f"/player {botName}{i} spawn at {position[0]} {botHeight} {position[2]-i*renderDistanceBlocks*2}")
+        minescript.execute(f"/player {botName}{i} spawn at {position[0]} {botHeight} {position[2]-(i*renderDistanceBlocks*2)-renderDistanceBlocks}")
         time.sleep(0.3)
 
 else: # something else
